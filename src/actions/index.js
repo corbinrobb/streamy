f1071c02721e7c11352fe7e0c7283ec758a8a723
Corbin Robb <corbin@Corbins-MacBook-Pro.local>
2021-02-12 14:26:11 -0700

    Work on more of the StreamsList componenet

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -17,9 +18,11 @@ export const signOut = () => {
   return { type: SIGN_OUT };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const fetchStream = (id) => async (dispatch) => {
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
@@ -7,12 +7,13 @@ import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
+import history from "../history";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Router>
+      <div className="ui container">
+        <Router history={history}>
           <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" component={StreamCreate} />
